cd2268c8a37076ce1d26c1b8fab1f90fc95e37af
Renat <renatakhmedyanov@gmail.com>
2022-11-09 19:12:36 +0400

    Add Task 30

---
+++ Task_30/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Программа, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке. Далее считает количество нулей и единиц, если единиц больше чем нулей то выводит TRUE на экран, иначе выводит False.");
+
+Console.Write("Введите количество элементов в массиве: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int[] GetArray(int N)
+{
+    int[] Array = new int[N];
+    Random random = new Random();
+    for (int i = 0; i < N; i++)
+    {
+        Array[i] = random.Next(0, 2);
+    }
+    return Array;
+}
+
+void PrintArray(int[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write(Array[i] + " ");
+    }
+}
+int[] Array = GetArray(N);
+Console.Write("Полученный массив: ");
+PrintArray(Array);
+Console.WriteLine();
+
+int countZero = 0;
+int countOne = 0;
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i] == 0)
+    {
+        countZero++;
+    }
+    else
+    {
+        countOne++;
+    }
+}
+Console.WriteLine(countOne > countZero);
