3dc452c5f66585b8c8340f14fdb12b3e84216dc7
Renat <renatakhmedyanov@gmail.com>
2022-11-09 21:08:37 +0400

    Add Task 26

---
+++ Task_26Hard/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Программа, которая принимает на вход целое или дробное число и выдаёт количество цифр в числе.");
+
+
+Console.WriteLine("Введите число");
+decimal input = decimal.Parse(Console.ReadLine()!);
+
+while (input % 1 > 0)
+{
+    input *= 10;
+}
+
+long newInput = (long)input;
+
+long GetNumberOfDigits(long newInput)
+{
+    int count = 0;
+    do
+    {
+        newInput /= 10;
+        count++;
+    } while (newInput > 0);
+    return count;
+}
+long result = GetNumberOfDigits(newInput);
+Console.WriteLine("Количество цифр в числе: " + result);
